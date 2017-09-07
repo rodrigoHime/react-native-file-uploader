d1e6dd5654a3b2d25e006a8e159fe80a3082aa48
keenubee <aleks.dev@gmail.com>
2017-09-07 13:17:41 +0300

    Update to latest RN codebase

--- android/src/main/java/com/farmisen/react_native_file_uploader/RCTFileUploaderPackage.java
+++ android/src/main/java/com/farmisen/react_native_file_uploader/RCTFileUploaderPackage.java
@@ -1,7 +1,7 @@
 /**
  * Copyright © 2016 Fabrice Armisen <farmisen@gmail.com>
  * This program is free software. It comes without any warranty, to
- * the extent permitted by applicable law.You can redistribute it and/or modify 
+ * the extent permitted by applicable law.You can redistribute it and/or modify
  * it under the terms of the Do What The Fuck You Want To Public License, Version 2,
  * as published by Sam Hocevar. See http://www.wtfpl.net/ for more details.
  */
@@ -25,11 +25,6 @@ public class RCTFileUploaderPackage implements ReactPackage {
     @Override
     public List<NativeModule> createNativeModules(ReactApplicationContext reactApplicationContext) {
         return Arrays.<NativeModule>asList(new RCTFileUploaderModule(reactApplicationContext));
-    }
-
-    @Override
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
-        return Collections.emptyList();
     }
 
     @Override
